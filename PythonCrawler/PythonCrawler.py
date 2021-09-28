6840a3d37040f765c306de40c528c13c9a7694c2
Vítor Mateus <vitor0mateus00@hotmail.com>
2021-09-28 17:29:03 -0300

    Add files via upload

--- PythonCrawler/PythonCrawler.py
+++ PythonCrawler/PythonCrawler.py
@@ -1,25 +1,35 @@
 #----------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------------------------
-#We use the bs4 feature to transform our html into a beautifulsoup object.
-#Usamos o recurso "bs4" para podermos transformar / "parsear" html's em objetos "beautifulsoup".
+
+#
+# Usei "json" para "despejar" a saída do respectivo script em arquivo .json.
 import json
+
+#
+# "unicodedata" foi usado por mim para limpar caracteres especiais nao regidos sob parametro "UTF-8".
+import unicodedata
+
+# We use the "bs4" feature to transform our html into a beautifulsoup object.
+# Usamos o recurso "bs4" para podermos transformar / "parsear" html's em objetos "beautifulsoup".
 from bs4 import BeautifulSoup
 
-#"pandas" will struct our data list.
-#"pandas" nos ajudará a estruturar nossas listas de dados .
+# "pandas" will help us organize our data list.
+# "pandas" nos ajudará a estruturar nossas listas de dados .
 import pandas as pd
 
-#The "requests" feature will "ask for permission" to the websites.
-#"requests" nos ajudarão a "pedir permissão" para entrar nos sites.
+# The "requests" feature will "ask for permission" to the websites.
+# "requests" nos ajudarão a "pedir permissão" para entrar nos sites.
 import requests
 
+# Wizard Terminate Function
 #----------------------------------------------------------------------------------------------------------------------
 def wizard_terminate():
     print('\n')
     print('Crawler Process Terminated!!!')
     quit()
 
+# Html Objectification Function
 #----------------------------------------------------------------------------------------------------------------------              
 def bs_objectify(url_to_objectify):   
 
@@ -34,10 +44,38 @@ def bs_objectify(url_to_objectify):
     if(urls_found == []):
         urls_found = url_parsed.findAll('p', attrs={'class': 'callout'})      
     
-
     return urls_found
 
-#Wizard behaviour
+# String Clean Up Function
+#----------------------------------------------------------------------------------------------------------------------
+def special_char_cleanup(string: str) -> str:
+    normalized  = unicodedata.normalize('NFD', string)
+    return normalized.encode('ascii', 'ignore').decode('utf8')
+
+# General Variables
+#----------------------------------------------------------------------------------------------------------------------
+
+# Abaixo está o site inicial, por onde todo o proceso partirá.
+start_url = 'https://www.gov.br/ana/pt-br/acesso-a-informacao/agenda-de-autoridades'
+
+# A variável em questão representa o site atual, dos vários sites que o "crawler" percorrerá.
+current_url = start_url
+
+# Uma lista com todos os sites úteis encontrados a partir da página inicial.
+urls_found_list = bs_objectify(current_url)
+
+# Booleana que retornará a escolha do usuário sobre a criação de um arquivo .json.
+create_json = False
+
+create_mongodb_lib = False
+
+# Criação de arquivos .html nesta pasta para visualização offline das urls acessadas (a ser implementada).
+#create_offline_view_url_list = False
+
+# Lista de TODOS os sites acessados para visualização offline (a ser implementada).
+#offline_view_list = []
+
+# Wizard Behaviour
 #----------------------------------------------------------------------------------------------------------------------
 print('Crawler Process Initialized!!! \n')
 
@@ -53,172 +91,206 @@ if(wizard_guide is None):
     print('Invalid Command!!! \n')
     wizard_terminate()    
 
-wizard_guide = input('Would you like to create a .json file? [y/n]\n')
+wizard_guide = input('Would you like to create .json files? [y/n]\n')
 print('\n')
 
 if(wizard_guide == "y"):
 
-    # json_create = True    
-    print('Warming up the .json file...')    
+    create_json = True    
+    print('Warming up the .json files...')    
     print('\n') 
+
+elif(wizard_guide == "n"):
+    print('No json file will be created!!!\n')
+    print('\n')  
 
 elif(wizard_guide is not None):
     print('Invalid Command!!! \n')
     wizard_terminate()
 
-elif(wizard_guide == "n"):
-    print('No json file will be created!!!\n')
-    print('\n')       
-
 
 print('Acquiring Necessary Data...\nThis May Take a While...')
 print('\n') 
 
-#General Variables
+# Structured Collections
 #----------------------------------------------------------------------------------------------------------------------
-start_url = 'https://www.gov.br/ana/pt-br/acesso-a-informacao/agenda-de-autoridades'
-
-current_url = start_url
-
-urls_found_list = bs_objectify(current_url)
-
-
-authority_data_list = []
-
-#Os agentes a serem encontrados pelo "crawler".
-agent_data_list = []
-
-#As agendas dos agentes a serem encontradas pelo "crawler".
-agent_agenda_data_list = []
-
-#----------------------------------------------------------------------------------------------------------------------
-
 structured_dict = {
     'Authority Type': '',
-    'Agent Name/Occupation': '[]',
+    'Agent Name/Occupation': '',    
     'Date': '',
     'Hour': '',
-    'Appointment place': ''
+    'Agent Link': ''
 }
 
-structured_dict_list = [{
+structured_dict_list = [
     
-}]
+]
 
 structured_list = []
 
-#Crawler Process
+# Crawler Process
 #----------------------------------------------------------------------------------------------------------------------
+# Para cada site de autoridade encontrado a partir do inicial, e
+# armazenado em uma lista, acessa o proximo site a ser analisado.
 for new_url in urls_found_list:
 
+    # Acha o site da autoridade encontrado dentro do site inicial 
+    # e o guarda nesta variável para ser acessada futuramente, 
+    # repete para cada autoridade encontrada.
     authority_data = new_url.find('a', attrs={'class': 'internal-link'}) 
 
-                    
-    if(authority_data) != None:
-        
-        
-        authority_data_list.append([authority_data.text, authority_data['href']])
-        #Nome/agenda da autoridade
-        #Link da autoridade                
 
-        agents_url_found_list = bs_objectify(authority_data['href']) 
+    # Se houver algum site encontrado, continua o processo...                
+    if(authority_data) != None:       
+                             
+        # Retorna uma lista com todos os sites
+        # encontrados dentro do site da autoridade 
+        # em questão.
+        agents_url_found_list = bs_objectify(authority_data['href'])        
         
-        structured_dict['Authority Type'] = authority_data.text
-        
+        #"Limpa" os caracteres especiais do nome da 
+        # autoridade e guarda esse novo dado nesta variável.
+        authority_data = special_char_cleanup(authority_data.text)
 
+        # Adiciona o nome da autoridade ao parametro  
+        # entre aspas simples correspondente no dicionário.
+        structured_dict['Authority Type'] = authority_data       
+
+
+        # Para cada site de agentes encontrado, a partir do site da autoridade, e
+        # armazenado em uma lista, acessa o proximo site a ser analisado (site com agentes).
         for new_agent_url in agents_url_found_list:
 
-            agent_data = new_agent_url.find('a', attrs={'class': 'internal-link'}) 
-            #Nome/cargo do agente
-            #Link do agente            
-
-            if(agent_data) != None:  
-                
-                agent_data_list.append([agent_data.text ,agent_data['href']]) 
+            # Guarda nesta variável o nome e a ocupação do agente encontrado no site com os agentes.
+            agent_data = new_agent_url.find('a', attrs={'class': 'internal-link'})                        
             
-                agent_appointment_url_found_list = bs_objectify(agent_data['href'])  
+            
+            if(agent_data) != None:                 
+                 
+                # Retorna uma lista com todos os sites objetificados
+                # encontrados dentro do site com o nome dos agente 
+                # em questão.
+                agent_appointment_found_list = bs_objectify(agent_data['href'])  
 
+                # No terminal, printa o dado que está sendo extraído e
+                # o link de onde ele está.
                 print('\n\nData Acquired: ' + agent_data.text) 
-                print('Accessing: ' + agent_data['href'])
-
-                structured_dict['Agent Name/Occupation'] = agent_data.text             
+                print('Accessing: ' + agent_data['href'])                         
                    
-                # print(structured_dict) 
+                # Adiciona o LINK para conferir a agenda do agente ao parametro  
+                # entre aspas simples correspondente no dicionário.
+                structured_dict['Agent Link'] = agent_data['href'] 
 
+                # "Limpa" os caracteres especiais do nome do agente e de sua 
+                # ocupação e guarda esse novo dado nesta variável.
+                agent_data = special_char_cleanup(agent_data.text)
 
-                for new_agent_appointment in agent_appointment_url_found_list:                    
+                # Adiciona o nome do AGENTE e sua OCUPAÇÃO ao parametro  
+                # entre aspas simples correspondente no dicionário.
+                structured_dict['Agent Name/Occupation'] = agent_data              
+                  
 
-                    agent_appointment_date = new_agent_appointment.find('li', attrs={'class': 'day is-selected has-appointment'})  
+                # Dentro do site objetificado do agente, o "crawler" retornará informações básicas.
+                for new_agent_appointment in agent_appointment_found_list:                    
 
-                    agent_appointment_data_local = new_agent_appointment.findAll('div', attrs={'class': 'item-compromisso'})                
+                    # Guarda nesta variável a estrutura "crua" onde está a informação da "data de hoje".
+                    agent_appointment_date = new_agent_appointment.find('li', attrs={'class': 'day is-selected has-appointment'})                     
+
+                    # Lista os compromissos encontrados pelo "crawler".
+                    agent_appointment_data_list = new_agent_appointment.findAll('div', attrs={'class': 'item-compromisso'})                
                               
-                     
-                    for agent_appointment_data in agent_appointment_data_local:
+                    # Para cada compromisso encontrado e listado, o "crawler" irá
+                    # extrair os dados úteis que ali estão.
+                    for agent_appointment_data in agent_appointment_data_list:
 
-                        agent_appointment_title = agent_appointment_data.find('h4', attrs={'class': 'compromisso-titulo'})                                               
+                        # Guarda em uma variável a estrutura "crua" onde está a informação do local de reunião.    
+                        # agent_appointment_title = agent_appointment_data.find('h4', attrs={'class': 'compromisso-titulo'})                                               
 
+                        # Guarda nesta variável a estrutura "crua" onde está a informação do local de reunião.
                         agent_appointment_place = agent_appointment_data.find('div', attrs={'class': 'compromisso-local'})
 
 
+                        # Se houver horário de início e término da reunião,
+                        # tais horários comporão a variável da hora, se não houver, 
+                        # apenas o horário de início aparecerá.
                         if(agent_appointment_data.find('time', attrs={'class': 'compromisso-inicio'}) and agent_appointment_data.find('time', attrs={'class': 'compromisso-fim'})) != None:
-
+                                
                             agent_appointment_hour = agent_appointment_data.find('time', attrs={'class': 'compromisso-inicio'}).get_text() + ' - ' + agent_appointment_data.find('time', attrs={'class': 'compromisso-fim'}).get_text()
-                        
+                                                
                         elif(agent_appointment_data.find('time', attrs={'class': 'compromisso-inicio'})) != None:
 
                             agent_appointment_hour = agent_appointment_data.find('time', attrs={'class': 'compromisso-inicio'}).get_text()
+                                                                    
 
-                        
-                        if(agent_appointment_date) is None:
+                        # Se houver DATA de início da reunião, adiciona tal valor 
+                        # ao parâmetro correspondente no dicionário.  
+                        if(agent_appointment_date) is not None:
+                                                  
+                            structured_dict['Date'] = agent_appointment_date['data-day']   
 
-                            agent_appointment_date = ''                             
+                        else:
+
+                            agent_appointment_date = ''  
                         
-                        
-                        if(agent_appointment_hour) is None:
+
+                        # Se houver HORA de início da reunião, adiciona tal valor 
+                        # ao parâmetro correspondente no dicionário.                                            
+                        if(agent_appointment_hour) is not None:
+
+                            structured_dict['Hour'] = agent_appointment_hour 
+
+                        else:
 
                             agent_appointment_hour = '' 
 
 
-                        if(agent_appointment_place) is not None: 
+                        # Se houver LOCAL DE REUNIÃO, o nome do local irá para 
+                        # o parâmetro correspondente no dicionário, se não houver,
+                        # uma mensagem aparecerá no local.
+                        if(agent_appointment_place) is not None:
 
-                            structured_list.append([authority_data.text, agent_data.text, agent_appointment_date['data-day'], agent_appointment_hour, agent_appointment_place.text])  
-                            structured_dict['Date'] = agent_appointment_date['data-day']
-                            structured_dict['Hour'] = agent_appointment_hour
-                            structured_dict['Appointment Place'] = agent_appointment_place.text 
+                            agent_appointment_place = special_char_cleanup(agent_appointment_place.text)
+                            structured_dict['Appointment Place'] = agent_appointment_place                           
+                            structured_list.append([authority_data, agent_data, agent_appointment_date['data-day'], agent_appointment_hour, agent_appointment_place])                                                  
+                                                     
+                        else: 
                             
+                            structured_dict['Appointment Place'] = 'Not Declared' 
+                            structured_list.append([authority_data, agent_data, agent_appointment_date['data-day'], agent_appointment_hour, 'Not Declared'])                      
 
-                        elif(agent_appointment_place) is None:
 
-                            structured_list.append([authority_data.text, agent_data.text, agent_appointment_date['data-day'], agent_appointment_hour, ''])
-                            structured_dict['Date'] = agent_appointment_date['data-day']
-                            structured_dict['Hour'] = agent_appointment_hour
-                            structured_dict['Appointment Place'] = '' 
+                        # Adiciona uma cópia do dicionário após todas as informações
+                        # necessárias terem sido inseridas e parte para adicionar o
+                        # dicionário do próximo agente.
+                        structured_dict_list.append(structured_dict.copy())                        
 
-                        structured_dict_list.append([structured_dict])
-                        date = agent_appointment_date['data-day']
-                        # print(structured_dict_list)                                                           
+                        # (test variable)
+                        date = agent_appointment_date['data-day'] 
 
-                        
-                    
-    
+                        # (hora da execução do "crawler" para compor o nome do arquivo .json)   
+                        #hour = ...                                                                           
 
+# List Tabulation                                               
 #----------------------------------------------------------------------------------------------------------------------
 structured_table = pd.DataFrame(structured_list, columns=['Authority Type', 'Authority Name/Occupation', 'Date', 'Hour', 'Appointment Place'])
-#----------------------------------------------------------------------------------------------------------------------
-# ... .to_json(str(agent_name) + 'Agenda' + '.json')
-# authority_table.to_json('Authorities.json')
-# agent_table.to_json('Agents.json')
-# agent_agenda_table.to_json('AgentsAgenda.json')
-structured_table.to_json('StructuredTable' + date + '.json')
-with open('StructuredDictList' + date + '.json', "w") as f:
-    json.dump(structured_dict_list, f)
 
+# .json Dump Archives
 #----------------------------------------------------------------------------------------------------------------------
-print(structured_list)
-# print(authority_table)
-# print(agent_table)
-# print(agent_agenda_table)
-print("\nCrawler Process Successfully Ended!!!")
+if(create_json) == True:
+
+    structured_table.to_json('StructuredTable' + date + '.json')
+
+    with open('StructuredDictList' + date + '.json', "w") as outfile:
+        json.dump(structured_dict_list, outfile, ensure_ascii=True)
+
+# MongoDB Dump Section
+#----------------------------------------------------------------------------------------------------------------------
+
+
+# Process Finalization
+#----------------------------------------------------------------------------------------------------------------------
+print("\nCrawler Process Ended!!!")
+
 #----------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------------------------
 #----------------------------------------------------------------------------------------------------------------------
